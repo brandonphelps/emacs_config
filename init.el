3189e4da739c9e0b4ad52732ecf6de07c4786156
brandon phelps <bwp44f@mst.edu>
2020-02-09 21:52:49 -0600

    added some mail configurations

--- init.el
+++ init.el
@@ -25,10 +25,11 @@
  '(custom-enabled-themes (quote (deeper-blue)))
  '(org-agenda-files
    (quote
-    ("~/agenda/emacs_learning.org" "~/agenda/food/food_to_try.org" "~/agenda/food/recipes.org" "~/agenda/car.org" "~/agenda/projects/westinsfriendsgame.org" "~/agenda/projects/gw2api.org" "~/agenda/video_games/league_of_legends.org" "~/agenda/refile.org")))
+    ("~/agenda/video_games/league_of_legends.org" "~/agenda/projects/emacs_hacking.org" "~/agenda/japanese/wanikani.org" "~/agenda/emacs_learning.org" "~/agenda/food/food_to_try.org" "~/agenda/food/recipes.org" "~/agenda/car.org" "~/agenda/projects/westinsfriendsgame.org" "~/agenda/projects/gw2api.org" "~/agenda/refile.org")))
  '(package-selected-packages
    (quote
     (tramp ggtags smart-tabs-mode neotree cmake-ide rust-mode yaml-mode magit lua-mode org-journal org-kanban)))
+ '(send-mail-function (quote mailclient-send-it))
  '(tramp-remote-process-environment
    (quote
     ("ENV=''" "TMOUT=0" "LC_CTYPE=''" "CDPATH=" "HISTORY=" "MAIL=" "MAILCHECK=" "MAILPATH=" "PAGER=cat" "autocorrect=" "correct="))))
@@ -42,14 +43,19 @@
 
 ;; agenda stuff
 
+(global-set-key (kbd "<f12>") 'org-agenda)
+
+
 (ido-mode t)
 
 ;; programming stuff
 
 ;; remote stuff
+(require 'tramp)
 (add-to-list 'tramp-remote-path "~/.local/bin")
 
 (smart-tabs-insinuate 'c 'c++ 'python)
+
 
 
 ;; compliation mode coloring 
@@ -73,6 +79,29 @@
   (setq tab-width 2)
   )
 (add-hook 'c++-mode-hook 'my-c-mode-hook)
+
+
+;; mail server stuff
+
+
+(setq send-mail-function 'smtpmail-send-it
+   message-send-mail-function 'smtpmail-send-it
+   smtpmail-starttls-credentials
+   '(("smtp.gmail.com" 587 nil nil))
+   smtpmail-auth-credentials
+   (expand-file-name "~/.authinfo")
+   smtpmail-default-smtp-server "smtp.gmail.com"
+   smtpmail-smtp-server "smtp.gmail.com"
+   smtpmail-smtp-service 587
+   smtpmail-debug-info t
+   starttls-extra-arguments nil
+   smtpmail-warn-about-unknown-extensions t
+   starttls-use-gnutls nil)
+
+
+;; example compose mail with filling out some stuff
+;; (compose-mail "bwp44f@mst.edu" "Food recipe" '(("From" . "zaszrules@gmail.com") ("" . "Hello World")))
+
 
 ;; (global-set-key (kbd "C-c i") 'windmove-up)
 ;; (global-set-key (kbd "C-c k") 'windmove-down)
