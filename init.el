d0d380e71598cff83edc34a5cc9c0ef29679a2b5
brandon phelps <bwp44f@mst.edu>
2021-10-28 03:12:48 -0500

    updated versions

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
 
+
+
 ;; straight bootstrap
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -60,12 +62,15 @@
 
 (use-package org-roam
   :custom
- (org-roam-directory "~/RoamNotes")
+  (org-roam-directory "~/RoamNotes")
+
   :bind (("C-c n l" . org-roam-buffer-toggle)
 	 ("C-c n f" . org-roam-node-find)
 	 ("C-c n i" . org-roam-node-insert))
   :config
   (org-roam-setup))
+
+(setq org-roam-v2-act t)
 
 (use-package rainbow-delimiters)
 (use-package ag)
